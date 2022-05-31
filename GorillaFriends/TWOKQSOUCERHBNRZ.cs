ac308c033fc7dab78030939ea2d3710d8211796f
fchb1239 <fchb1239.dk@gmail.com>
2022-05-31 15:20:20 +0200

    updated version number

--- GorillaFriends/TWOKQSOUCERHBNRZ.cs
+++ GorillaFriends/TWOKQSOUCERHBNRZ.cs
@@ -2,5 +2,5 @@ class TWOKQSOUCERHBNRZ
 {
     public const string BWOKRIVJRA = "net.rusjj.gorillafriends";
     public const string BWOKTMFTZQ = "Gorilla Friends!";
-    public const string BWOKVMVYCSIVBG = "1.1.0";
+    public const string BWOKVMVYCSIVBG = "1.1.1";
 }
