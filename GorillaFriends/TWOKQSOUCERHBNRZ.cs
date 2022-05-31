3c64559f5b775b653b125d0a124f6c8f2c1aee8d
fchb1239 <fchb1239.dk@gmail.com>
2022-05-31 10:12:34 +0200

    browsseeerrr momenttooo

---
+++ GorillaFriends/TWOKQSOUCERHBNRZ.cs
@@ -0,0 +1,6 @@
+class TWOKQSOUCERHBNRZ
+{
+    public const string modGUID = "net.rusjj.gorillafriends";
+    public const string modName = "Gorilla Friends!";
+    public const string modVersion = "1.1.0";
+}
